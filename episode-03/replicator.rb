f87c7ebb5c2681b017555315ed41a882aabf40ea
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-03 15:16:29 -0400

    episodes 1-3

--- episode-03/replicator.rb
+++ episode-03/replicator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Replicator
 
   attr_reader :plate
@@ -49,6 +51,7 @@ class Replicator
 
   def adjust_temperature
     return unless glass_in_tummy
+
 
     glass_in_reactor_core = @enterprise.transporter.energize(obj: glass_in_tummy, from: @tummy, to: @enterprise.reactor.core)
 
